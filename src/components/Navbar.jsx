1b749626b43957829d8c1fb76257447ac82a92d4
David <darevalo-2022062@kinal.edu.gt>
2024-05-21 14:14:52 -0600

    BarraModif

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,26 @@ export const Navbar = () => {
     return (
         <header class="cabecera">
             <h1 class="cabecera-h1">
-                <a href="#">
+                <Link href="#">
                     <img src="https://i.ibb.co/dW1PPhz/Icon-Garden-Travel-Hotel.png" border="0" alt="Logo-Garden" />
-                    Garden Travel Hotel
-                </a>
+                    Garden Travel
+                </Link>
             </h1>
             <nav class="cabecera-nav">
                 <ul class="cabecera-ul">
-                    <li class="cabecera-li"><a href="#">Hotels</a></li>
-                    <li class="cabecera-li"><a href="#">Reservations</a></li>
-                    <li class="cabecera-li"><a href="#">Services</a></li>
+                    <li class="cabecera-li"><Link href="#">Hotels</Link></li>
+                    <li class="cabecera-li"><Link href="#">Reservations</Link></li>
+                    <li class="cabecera-li"><Link href="#">Services</Link></li>
                 </ul>
             </nav>
             <nav class="cabecera-nav">
                 <ul class="cabecera-ul">
                     <li class="cabecera-li">
-
-                        <a href="#">Login</a>
+                        <Link href="#">Login</Link>
                     </li>
-                    <li class="cabecera-li"><a href="#">Logout</a></li>
+                    <li class="cabecera-li">
+                        <Link href="#">Logout</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
