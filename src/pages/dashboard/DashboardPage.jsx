b0ce2f2c50319ed2576ec8cd23f4ff713e7f2ba8
David <darevalo-2022062@kinal.edu.gt>
2024-05-21 15:32:16 -0600

    Home_Page

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,13 +1,20 @@
 
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import {Navbar} from '../../components/Navbar'
+import { Home } from '../../components/Home'
 import './dashboardPage.css'
+
 export const DashboardPage = () => { 
     return(
         <>
             <Navbar />
-            <div>
-                
-            </div>
+            <Routes>
+                <Route path="/" element={<><Home/></>} />
+                <Route path="hotels" element={<h1>Hotels</h1>} />
+                <Route path="reservations" element={<h1>Reservations</h1>} />
+                <Route path="services" element={<h1>Services</h1>} />
+            </Routes>
         </>
     )
 }
